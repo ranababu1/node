33f387da8a430948927ef69fba7a8566b541a36b
ranababu1 <imurokks@gmail.com>
2020-05-14 02:56:43 +0530

    files added

--- app.js
+++ app.js
@@ -1,10 +1,38 @@
-const http = require('http');
-const port = process.env.PORT || 3000
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Hi there!</h1>');
+var http = require('http');
+var fs = require('fs');
+var server = http.createServer(function (req, resp) {
+    if (req.url === "/") {
+        fs.readFile("home/home.html", function (error, pgResp) {
+            if (error) {
+                resp.writeHead(404);
+                resp.write('Contents you are looking are Not Found');
+            } else {
+                resp.writeHead(200, { 'Content-Type': 'text/html' });
+                resp.write(pgResp);
+            }
+
+            resp.end();
+        });
+    }
+    else if (req.url === "/status") {
+        fs.readFile("status/status.html", function (error, pgResp) {
+            if (error) {
+                resp.writeHead(404);
+                resp.write('Contents you are looking are Not Found');
+            } else {
+                resp.writeHead(200, { 'Content-Type': 'text/html' });
+                resp.write(pgResp);
+            }
+
+            resp.end();
+        });
+    } else {
+        //4.
+        resp.writeHead(200, { 'Content-Type': 'text/html' });
+        resp.write('<h1>Product Manaager</h1><br /><br />To create product please enter: ' + req.url);
+        resp.end();
+    }
 });
-server.listen(port, () => {
-    console.log(`Server running at port ` + port);
-});
+server.listen(1805);
+
+console.log('Server Running');
